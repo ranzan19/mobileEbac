f95e01f9fa78dc42f39316e2c09cb573e184cec2
ranzan19 <rafaspirina@gmail.com>
2023-09-01 23:22:19 -0300

    add scriptableobjects and colors
    - criado scriptableobjects para criação dos levels e ArtManager
    - randomização das cores de cada level (ColorManager)

--- mobileEbac/Assets/Scripts/LevelManager/LevelManager.cs
+++ mobileEbac/Assets/Scripts/LevelManager/LevelManager.cs
@@ -8,12 +8,33 @@ public class LevelManager : MonoBehaviour
 
     public List<GameObject> levels;
 
+    public List<LevelPieceBasedSetup> levelPieceBasedSetups;
+
+    /*Sem ScriptableObjects
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPiecesStart;
+    public List<LevelPieceBase> levelPieces;
+    public List<LevelPieceBase> levelPiecesEnd;
+    public int piecesStartNumber = 2;
+    public int piecesNumber = 5;
+    public int piecesEndNumber = 1;*/
+
+    public float timeBetweenPieces = .3f;
+
     [SerializeField] private int _index;
     private GameObject _currentLevel;
 
+
+    [SerializeField] private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
+    private LevelPieceBasedSetup _currSetup;
+
+
+
+
     private void Awake()
     {
-        SpawnNextLevel();
+        //SpawnNextLevel();
+        CreateLevelPieces();
     }
 
     private void SpawnNextLevel()
@@ -39,11 +60,99 @@ public class LevelManager : MonoBehaviour
         _index = 0;
     }
 
+    #region
+    private void CreateLevelPieces()
+    {
+        CleanSpawnedPieces();
+
+        if (_currSetup != null)
+        {
+            _index++;
+
+            if (_index >= levelPieceBasedSetups.Count)
+            {
+                ResetLevelIndex();
+            }
+        }
+
+        _currSetup = levelPieceBasedSetups[_index];
+
+        for (int i = 0; i < _currSetup.piecesStartNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPiecesStart);
+        }
+
+        for (int i = 0; i < _currSetup.piecesNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPieces);
+        }
+
+        for (int i = 0; i < _currSetup.piecesEndNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPiecesEnd);
+        }
+
+        ColorManager.Instance.ChangeColorByType(_currSetup.artType);
+
+
+    }
+    
+    
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = list[Random.Range(0, list.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+        else
+        {
+            spawnedPiece.transform.localPosition = Vector3.zero;
+        }
+
+        foreach(var p in spawnedPiece.GetComponentsInChildren<ArtPiece>())
+        {
+            p.ChangePiece(ArtManager.Instance.GetSetupByType(_currSetup.artType).gameObject);
+        }
+
+        _spawnedPieces.Add(spawnedPiece);
+
+    }
+
+
+    private void CleanSpawnedPieces()
+    {
+        for(int i = _spawnedPieces.Count - 1; i >= 0; i--)
+        {
+            Destroy(_spawnedPieces[i].gameObject);
+        }
+
+        _spawnedPieces.Clear();
+    }
+
+
+    IEnumerator CreateLevelPiecesCoroutine()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < _currSetup.piecesNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPieces);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+    }
+    #endregion
+
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.D))
         {
-            SpawnNextLevel();
+            CreateLevelPieces();
         }
     }
 }
